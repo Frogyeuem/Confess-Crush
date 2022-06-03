df17b53d38bf6bb53aeb930c64521fc5f6055244
Frogyeuem <106756672+Frogyeuem@users.noreply.github.com>
2022-06-04 00:04:06 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
-    titleWeb: "Yêu Hương 3000 <3",
-    introTitle: 'Đinh Tử Hương',
+    titleWeb: "Yêu Trang 3000 <3",
+    introTitle: 'Trang Lê',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
+     Đằng sau chữ yêu đây là thương`,
+    btnIntro: '^^love you^^',
     title: 'Phải chăng anh đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng anh đã yêu em ngay từ khi nhìn thấy nụ cười ấy🥰 làm người yêu anh không',
-    btnYes: 'Có nha <33',
-    btnNo: 'Không nha :3',
+    desc: 'Phải chăng anh đã yêu em ngay từ khi nhìn thấy nụ cười ấy🥰',
+    btnYes: 'Yêu anh <33',
+    btnNo: 'Không yêu :3',
     question: 'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Cuối tuần sau,anh đón em đi tà tữa sau lễ nhé ^^.',
+    messDesc: 'Gặp nhau,anh đưa em đi tà tữa nhé ^^.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://www.youtube.com/watch?v=QzAB0B8XMIY'
 }
